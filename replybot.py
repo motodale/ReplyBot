a14dd43e3de015ef04989026a41e1525ebeee029
Voussoir <edalool@yahoo.com>
2014-05-20 15:27:52 -0700

    replybot 2

--- replybot.py
+++ replybot.py
@@ -14,7 +14,7 @@ PASSWORD  = bot.getp()
 #This is the bot's Password. 
 USERAGENT = bot.geta()
 #This is a short description of what the bot does. For example "Newsletter bot"
-SUBREDDIT = "test"
+SUBREDDIT = "all"
 #This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
 PARENTSTRING = "I'm hungry"
 #This is the word that you want to reply to
