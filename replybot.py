74d41b30e39681e23a666fafcd9b2c7de176700e
Voussoir <edalool@yahoo.com>
2014-05-20 15:26:10 -0700

    replybot 1

---
+++ replybot.py
@@ -0,0 +1,69 @@
+import praw # simple interface to the reddit API, also handles rate limiting of requests
+import time
+import os
+import sys
+import sqlite3
+try:
+    import bot #This is a file in my python library which contains my Bot's username and password. I can push code to Git without showing credentials
+except ImportError:
+    pass
+
+USERNAME  = bot.getu()
+#This is the bot's Username. In order to send mail, he must have some amount of Karma.
+PASSWORD  = bot.getp()
+#This is the bot's Password. 
+USERAGENT = bot.geta()
+#This is a short description of what the bot does. For example "Newsletter bot"
+SUBREDDIT = "test"
+#This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
+PARENTSTRING = "I'm hungry"
+#This is the word that you want to reply to
+REPLYSTRING = "Hi hungry, I'm dad"
+#This is the word you want to put in reply
+MAXPOSTS = 100
+#This is how many posts you want to retreieve all at once. Max 100, but your subs probably don't get 100 posts per minute.
+WAIT = 20
+#This is how many seconds you will wait between cycles. The bot is completely inactive during this time.
+WAITS = str(WAIT)
+#Do not edit this part
+
+sql = sqlite3.connect('sql.db')
+print('Loaded SQL Database')
+cur = sql.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
+print('Loaded Completed table')
+
+
+sql.commit()
+
+r = praw.Reddit(USERAGENT)
+r.login(USERNAME, PASSWORD) 
+
+def scanSub():
+    print('Searching '+ SUBREDDIT + '.')
+    subreddit = r.get_subreddit(SUBREDDIT)
+    posts = subreddit.get_comments(limit=MAXPOSTS)
+    for post in posts:
+        pid = post.id
+        pauthor = post.author.name
+        cur.execute('SELECT * FROM oldposts WHERE ID="%s"' % pid)
+        if not cur.fetchone():
+            try:
+                print('Found new comment: ' + pid + ' by ' + pauthor)
+                cur.execute('INSERT INTO oldposts VALUES("%s")' % pid)
+                pbody = post.body.lower()
+                if PARENTSTRING in pbody:
+                    print('\t ^found parenstring. Replying to ' + pauthor)
+                    post.reply(REPLYSTRING)
+
+            except IndexError:
+                pass
+    sql.commit()
+
+
+while True:
+    scanSub()
+    print('Running again in ' + WAITS + ' seconds \n')
+    sql.commit()
+    time.sleep(WAIT)
